6b8e8ed9d2dc8ee1a552f579e875becd5e0c2659
ameryakubov <alex_meryakubov@mail.ru>
2023-07-30 13:40:28 +0300

    "rework2"

--- src/main/java/ru/meryakubov/JavaBasicsLesson.java
+++ src/main/java/ru/meryakubov/JavaBasicsLesson.java
@@ -9,46 +9,46 @@ public class JavaBasicsLesson {
         boolean isFalse = false;
         byte a = -128;
         byte b = 127;
-        short a = -32768;
-        short b = 32767;
-        int a = -2147483648;
-        int b = 2147483647;
-        long a = –9223372036854775808;
-        long b = 9223372036854775807;
-        float a = 8.5F;
-        float b = -3.2F;
-        double a = 8.5;
-        double b = -3.2;
-        char a = 'a';
+        short c = -32768;
+        short d = 32767;
+        int e = -2147483648;
+        int f = 2147483647;
+        long g = -9223372036854775808L;
+        long h = 9223372036854775807L;
+        float i = 8.5F;
+        float j = -3.2F;
+        double k = 8.5;
+        double l = -3.2;
+        char m = 'a';
         String name = "Aleksey";
 
         // 1) поупражняться с математическими и логическими операторами, добиться переполнения при вычислениях,
         // посмотреть результаты (вывести в консоль)
 
         // математические операторы
-        int a = 30;
-        int b = 20;
-        int c = 35;
+        int n = 30;
+        int o = 20;
+        int p = 35;
 
-        System.out.println("a + b = " + (a + b));
-        System.out.println("a - b = " + (a - b));
-        System.out.println("a * b = " + (a * b));
-        System.out.println("b / a = " + (a / b));
-        System.out.println("b % a = " + (b % a));
-        System.out.println("c % a = " + (c % a));
-        System.out.println("a++   = " +  (a++));
-        System.out.println("b--   = " +  (--a));
+        System.out.println("n + o = " + (n + o));
+        System.out.println("n - o = " + (n - o));
+        System.out.println("n * o = " + (n * o));
+        System.out.println("o / n = " + (n / o));
+        System.out.println("o % n = " + (b % a));
+        System.out.println("p % n = " + (p % n));
+        System.out.println("n++   = " +  (n++));
+        System.out.println("o--   = " +  (--n));
 
         //логические операторы
-        boolean a = true;
-        boolean b;
-        b = a || true; // b истинно
-        b = !b; // b ложно
+        boolean q = true;
+        boolean r;
+        r = q || true; // r истинно
+        r = !r; // r ложно
         System.out.println(b); // выведет false
-        a = a || b; // a истинно
-        boolean c;
-        c = a && (a||b); //с истинно
-        System.out.println(c); // выведет true
+        q = q || r; // a истинно
+        boolean s;
+        s = q && (q||r); //с истинно
+        System.out.println(s); // выведет true
 
         // добиться переполнения
         int maxValue = Integer.MAX_VALUE;
